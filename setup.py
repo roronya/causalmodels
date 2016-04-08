fd2da692ad13282e7dd8fa78491ad338bfac9af3
roronya <yukikanai0204@gmail.com>
2016-04-08 10:12:59 +0900

    v0.1.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='causalmodels',
-    version='0.1.1',
+    version='0.1.2',
     description='Causal models in Python',
     long_description=long_description,
     url='http://github.com/roronya/causalmodels',
@@ -18,13 +18,13 @@ setup(
     author_email='roronya628@gmail.com',
     license='MIT',
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Libraries',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: Mathematics'
     ],
-    keywords='causality machine	learning',
+    keywords='causality machine	learning causalmodels',
     packages=find_packages(exclude=['tests*']),
     install_requires=['numpy', 'scipy', 'graphviz'],
 )
