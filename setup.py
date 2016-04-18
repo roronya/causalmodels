756841beb5db00227978e90686a7ec922916df27
roronya <yukikanai0204@gmail.com>
2016-04-18 18:42:19 +0900

    v0.1.7

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='causalmodels',
-    version='0.1.6',
+    version='0.1.7',
     description='Causal models in Python',
     long_description=long_description,
     url='http://github.com/roronya/causalmodels',
