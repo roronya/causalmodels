7f4ec7838421225ead923147822541c6569fd482
roronya <yukikanai0204@gmail.com>
2016-04-18 18:41:10 +0900

    [modify] add normalize

--- causalmodels/lingam.py
+++ causalmodels/lingam.py
@@ -5,7 +5,9 @@ from .interface import ModelInterface
 from .result import Result
 from .exception import *
 
+
 class DirectLiNGAM(ModelInterface):
+
     def __init__(self):
         self.order = None
         self.matrix = None
@@ -13,6 +15,7 @@ class DirectLiNGAM(ModelInterface):
     """
     implement of DirectLiNGAM
     """
+
     def fit(self, data, labels=None):
         #<!-- inner function
         def T_kernel(x_j, X, K):
@@ -31,7 +34,8 @@ class DirectLiNGAM(ModelInterface):
             # グラム行列のセンタリング
             def centering_gram_matrix(X):
                 n = X.shape[0]
-                P = np.array([[-1/n if i !=j else (n-2)/n for j in range(n)] for i in range(n)])
+                P = np.array(
+                    [[-1 / n if i != j else (n - 2) / n for j in range(n)] for i in range(n)])
                 return P.dot(X).dot(P)
 
             K_1 = centering_gram_matrix(K_1)
@@ -39,12 +43,14 @@ class DirectLiNGAM(ModelInterface):
 
             # K_k の作成
             n = K_1.shape[0]
-            K_k_left_upper = (lambda x: np.dot(x, x))(K_1 + n*kappa/2*np.eye(n))
+            K_k_left_upper = (lambda x: np.dot(x, x))(
+                K_1 + n * kappa / 2 * np.eye(n))
             K_k_right_upper = np.dot(K_1, K_2)
             K_k_left_lower = K_k_right_upper.copy()
-            K_k_right_lower = (lambda x: np.dot(x, x))(K_2 + n*kappa/2*np.eye(n))
+            K_k_right_lower = (lambda x: np.dot(x, x))(
+                K_2 + n * kappa / 2 * np.eye(n))
             K_k = np.concatenate([np.concatenate([K_k_left_upper, K_k_right_upper], axis=1),
-                                np.concatenate([K_k_left_lower, K_k_right_lower], axis=1)])
+                                  np.concatenate([K_k_left_lower, K_k_right_lower], axis=1)])
 
             # D_k の作成
             D_k_left_upper = K_k_left_upper.copy()
@@ -52,41 +58,45 @@ class DirectLiNGAM(ModelInterface):
             D_k_left_lower = np.zeros((n, n))
             D_k_right_lower = K_k_right_lower.copy()
             D_k = np.concatenate([np.concatenate([D_k_left_upper, D_k_right_upper], axis=1),
-                                np.concatenate([D_k_left_lower, D_k_right_lower], axis=1)])
+                                  np.concatenate([D_k_left_lower, D_k_right_lower], axis=1)])
 
             # np.linalg.det では nan になってしまうので行列式の計算時から log をとる
             def log_determinant(X):
                 U = sp.linalg.lu(X, permute_l=True)[1]
                 U = np.diag(U)
-                U = np.abs(U) # マイナスの値の log を計算するのを防ぐ
+                U = np.abs(U)  # マイナスの値の log を計算するのを防ぐ
                 U = np.log(U).sum()
                 return U
 
             log_K_k = log_determinant(K_k)
             log_D_k = log_determinant(D_k)
-            return (-1/2) * (log_K_k - log_D_k)
+            return (-1 / 2) * (log_K_k - log_D_k)
         # inner function --!>
 
         X = data.copy()
+        for i in range(X.shape[0]):
+            X[i] = (X[i] - X[i].mean()) / X[i].std()
         K = []
         n = data.shape[0]
         B = np.zeros((n, n))
         for k, x_k in enumerate(X):
             # 最も独立な変数を見つける
             x_m_index = np.argmin([T_kernel(x_j, X, K) if j not in K else float('inf')
-                                           for j, x_j in enumerate(X)])
+                                   for j, x_j in enumerate(X)])
             K.append(x_m_index)
 
             # 最も独立な変数から残りの変数への影響を計算する
             x_m = X[x_m_index]
-            for l , x_l in enumerate(X):
-                if l not in K:
-                    B[l][x_m_index] = np.cov(x_l, x_m, bias=1)[0][1] / np.var(x_m)
-
-            # 残りの変数から最も独立だった変数の影響を取り除く 
             for l, x_l in enumerate(X):
                 if l not in K:
-                    X[l] = x_l - (np.cov(x_m, x_l, bias=1)[0][1] / np.var(x_m)) * x_m
+                    B[l][x_m_index] = np.cov(x_l, x_m, bias=1)[
+                        0][1] / np.var(x_m)
+
+            # 残りの変数から最も独立だった変数の影響を取り除く
+            for l, x_l in enumerate(X):
+                if l not in K:
+                    X[l] = x_l - (np.cov(x_m, x_l, bias=1)
+                                  [0][1] / np.var(x_m)) * x_m
 
         self.order = K
         self.matrix = B
@@ -99,8 +109,10 @@ class DirectLiNGAM(ModelInterface):
             raise NotYetFitError()
         return Result(order=self.order, matrix=self.matrix, labels=self.labels)
 
+
 class SparseDirectLiNGAM(DirectLiNGAM):
-    def fit(self, data, labels, threshold = 0.2):
+
+    def fit(self, data, labels, threshold=0.2):
         super().fit(data, labels)
 
         # B の閾値以下の値を 0 設定する
@@ -111,7 +123,7 @@ class SparseDirectLiNGAM(DirectLiNGAM):
                     B[i][j] = 0
 
         # 直接的因果関係と間接的因果関係の両方を持つノードを発見する
-        A = B.astype(bool) # 隣接行列
+        A = B.astype(bool)  # 隣接行列
         indirect_effect = np.zeros(B.shape).astype(bool)
         A_n = A.copy()
         for n in range(len(A) - 1):
@@ -121,15 +133,17 @@ class SparseDirectLiNGAM(DirectLiNGAM):
         # 因果効果を計算し直す
         X = data.copy()
         K = self.order
-        for i,j in [(i, j) for i in K for j in K if i != j]:
+        for i, j in [(i, j) for i in K for j in K if i != j]:
             if indirect_effect[i][j]:
                 x_i = X[i]
                 for k in K:
                     if k != i and k != j and B[i][k] > 0 and not indirect_effect[i][k]:
                         x_k = X[k]
-                        x_i = x_i - (np.cov(x_i, x_k)[0][1] / np.var(x_k)) * x_k
+                        x_i = x_i - (np.cov(x_i, x_k)
+                                     [0][1] / np.var(x_k)) * x_k
                 x_j = X[j]
                 B[i][j] = np.cov(x_i, x_j, bias=1)[0][1] / np.var(x_j)
-                if B[i][j] < 0.2: B[i][j] = 0
+                if B[i][j] < 0.2:
+                    B[i][j] = 0
         self.matrix = B
         return self.predict()
--- tests/test_lingam.py
+++ tests/test_lingam.py
@@ -3,7 +3,7 @@ import causalmodels as cm
 
 a = np.random.laplace(size=100)
 b = np.random.laplace(size=100) + a
-c = np.random.laplace(size=100) + b
+c = np.random.laplace(size=100) + a + b
 data = np.array([a,b,c])
 labels = ['a','b','c']
 
