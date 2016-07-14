c5119affba31b6f8f84f11ddd9f1dd03d3a708c7
roronya <yukikanai0204@gmail.com>
2016-07-14 14:13:33 +0900

    change test

--- tests/test_svarlingam.py
+++ tests/test_svarlingam.py
@@ -2,15 +2,19 @@ import numpy as np
 import pandas as pd
 import causalmodels as cm
 
-a = np.random.laplace(size=100)
-b = np.random.laplace(size=100) + a
-c = np.random.laplace(size=100) + a + b
-b[1:] = b[1:] + a[:-1]
-c[2:] = c[2:] + b[:-2] + a[:-2]
-data = pd.DataFrame({"a":a, "b": b, "c": c})
+a = 0.5
+b = 1
+v1org = np.random.laplace(size=102)
+v2org = np.random.laplace(size=102)
+v3org = np.random.laplace(size=102)
+v1 = v1org[2:] + a * v1org[:-2] + b * v1org[1:-1]
+v2 = v2org[2:] + a * v2org[:-2] + b * v2org[1:-1] + a * v1org[:-2] + b * v1org[2:]
+v3 = v3org[2:] + a * v3org[:-2] + b * v3org[1:-1]
+data = pd.DataFrame({"v1": v1, "v2": v2, "v3": v3})
+#data = (data - data.mean()) / data.std()
 model = cm.SVARDirectLiNGAM(data.values, data.columns)
-result = model.fit(regression="lasso")
+result = model.fit(regression="lasso", maxlags=2)
 print(result.instantaneous_order)
 print(result.matrixes)
-result.plot(threshold=0.3)
-result.plot(threshold=0.3, separate=True)
+result.plot(threshold=0.2)
+#result.plot(threshold=0.3, separate=True)
