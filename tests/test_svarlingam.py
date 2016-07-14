febadd580973d60a349da79c1decd07d37d0417b
roronya <yukikanai0204@gmail.com>
2016-07-14 16:18:02 +0900

    change plot

--- tests/test_svarlingam.py
+++ tests/test_svarlingam.py
@@ -11,10 +11,9 @@ v1 = v1org[2:] + a * v1org[:-2] + b * v1org[1:-1]
 v2 = v2org[2:] + a * v2org[:-2] + b * v2org[1:-1] + a * v1org[:-2] + b * v1org[2:]
 v3 = v3org[2:] + a * v3org[:-2] + b * v3org[1:-1]
 data = pd.DataFrame({"v1": v1, "v2": v2, "v3": v3})
-#data = (data - data.mean()) / data.std()
+data = (data - data.mean()) / data.std()
 model = cm.SVARDirectLiNGAM(data.values, data.columns)
 result = model.fit(regression="lasso", maxlags=2)
 print(result.instantaneous_order)
 print(result.matrixes)
 result.plot(threshold=0.2)
-#result.plot(threshold=0.3, separate=True)
