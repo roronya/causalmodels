63d4b9e4839fb62ab1092d64c863a33c024ae724
roronya <yukikanai0204@gmail.com>
2016-07-14 16:25:52 +0900

    ddof=True

--- tests/test_svarlingam.py
+++ tests/test_svarlingam.py
@@ -11,7 +11,7 @@ v1 = v1org[2:] + a * v1org[:-2] + b * v1org[1:-1]
 v2 = v2org[2:] + a * v2org[:-2] + b * v2org[1:-1] + a * v1org[:-2] + b * v1org[2:]
 v3 = v3org[2:] + a * v3org[:-2] + b * v3org[1:-1]
 data = pd.DataFrame({"v1": v1, "v2": v2, "v3": v3})
-data = (data - data.mean()) / data.std()
+data = (data - data.mean()) / data.std(ddof=False)
 model = cm.SVARDirectLiNGAM(data.values, data.columns)
 result = model.fit(regression="lasso", maxlags=2)
 print(result.instantaneous_order)
